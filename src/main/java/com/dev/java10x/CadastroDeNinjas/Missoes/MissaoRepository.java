0a275488df37b681082dd1c75f59b1408d5d7b3d
edurxmos <eduardoramos15975@gmail.com>
2024-11-02 00:15:16 -0300

    feat: NinjaRepository e MissaoRepository conectados ao JPA

---
+++ src/main/java/com/dev/java10x/CadastroDeNinjas/Missoes/MissaoRepository.java
@@ -0,0 +1,6 @@
+package com.dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
+}
